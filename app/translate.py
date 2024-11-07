b79b8e596dc35e2793452b57302ab4f308a1c800
Messef <85040583+AkTewari123@users.noreply.github.com>
2024-11-07 17:42:12 -0500

    made this in docker!!!

--- app/translate.py
+++ app/translate.py
@@ -1,11 +1,5 @@
-import googletrans
+from deep_translator import GoogleTranslator
 import random
-from googletrans import Translator
-import time
-from pprint import pprint
-
-# Gets things lost in translation
-translator = Translator()
 
 
 def scraggle(z: int, txt: str):
@@ -16,68 +10,36 @@ def scraggle(z: int, txt: str):
     retArray = []
     languages = []
     offLimits = ["en", "zh-cn", "zh-tw"]
-    language = random.choice(list(googletrans.LANGUAGES.keys()))
+
+    # Create an instance of GoogleTranslator
+    translator = GoogleTranslator()
+    language_list = translator.get_supported_languages(as_dict=True)
+
+    # Select a random language for the first translation
+    language = random.choice(list(language_list.keys()))
+    while language in offLimits:
+        language = random.choice(list(language_list.keys()))
     offLimits.append(language)
-    try:
-        x = translator.translate(txt, dest=language)
-        offLimits.append(language)
-    except KeyError:
-        print("Try different text")
-        scraggle(z)
-    try:
-        og = googletrans.LANGUAGES[translator.detect(x.text).lang].capitalize()
-    except KeyError:
-        language = random.choice(list(googletrans.LANGUAGES.keys()))
-        while language == "zh-CN" or language == "zh-TW":
-            language = random.choice(list(googletrans.LANGUAGES.keys()))
-        try:
-            og = googletrans.LANGUAGES[translator.detect(x.text).lang].capitalize()
-        except KeyError:
-            language = random.choice(list(googletrans.LANGUAGES.keys()))
-    src = googletrans.LANGUAGES[language].capitalize()
-    item = f"\nEnglish -> {src}<br/>"
-    languages.append(item)
-    item += f"{x.origin} -> {x.text}\n"
+
+    # Translate from English to the chosen language
+    x = GoogleTranslator(source="en", target=language).translate(txt)
+    item = f"English -> {language.capitalize()}\n{x}\n"
     retArray.append(item)
-    for i in range(z - 1):
-        language = random.choice(list(googletrans.LANGUAGES.keys()))
-        while (
-            language in offLimits
-            or language == "chinese (simplified)"
-            or language not in googletrans.LANGUAGES.keys()
-        ):
-            language = random.choice(list(googletrans.LANGUAGES.keys()))
+    prevLanguage = language
+    prevSentence = x
+    # Apply additional translations for the 'scraggle' effect
+    for _ in range(z - 1):
+        language = random.choice(list(language_list.keys()))
+        while language in offLimits:
+            language = random.choice(list(language_list.keys()))
         offLimits.append(language)
-        try:
-            og = googletrans.LANGUAGES[translator.detect(x.text).lang].capitalize()
-        except KeyError:
-            language = random.choice(list(googletrans.LANGUAGES.keys()))
-            og = googletrans.LANGUAGES[translator.detect(x.text).lang].capitalize()
-        x = translator.translate(x.text, dest=language)
-        src = googletrans.LANGUAGES[language].capitalize()
 
-        item = f"{og} -> {src}<br/>"
-        languages.append(item)
+        # Translate text to the chosen language and back to English
+        x = GoogleTranslator(source=prevLanguage, target=language).translate(txt)
+        back_to_english = GoogleTranslator(source=language, target="en").translate(x)
 
-        # time.sleep(0.5)
-        item += f"{x.origin} -> {x.text}<br/>"
-
-        engVer = translator.translate(x.text, dest="en")
-        item += f"English version: {engVer.text}\n"
+        item = f"{prevLanguage.capitalize()}: {prevSentence}\n\n{language.capitalize()}: \n{x}\n\nEnglish:{back_to_english}\n"
         retArray.append(item)
-    try:
-        og = googletrans.LANGUAGES[translator.detect(x.text).lang].capitalize()
-    except KeyError:
-        og = "Chinese"
-    x = translator.translate(x.text, dest="en")
-    item = f"{og} -> English<br/>"
-    languages.append(item)
-
-    # time.sleep(0.5)
-    item += f"{x.origin} -> {x.text}<br/>"
-    retArray.append(item)
-
-    # time.sleep(2)
-    item = f"\n\nOriginal: {txt}<br/>Scrambled: {x.text}\n"
-    retArray.append(item)
+        prevLanguage = language
+        prevSentence = x
     return retArray, languages
